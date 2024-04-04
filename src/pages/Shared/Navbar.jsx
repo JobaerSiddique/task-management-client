8f77e3ad1bb59d8dade0ab31bcde2fc068d2a0e0
JobaerSiddique <jobaersiddique28me@gmail.com>
2024-04-04 23:49:22 +0600

    nav&footer

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -25,27 +25,27 @@ const Navbar = () => {
     const menuItem = <>
    
          
-          <li><Link to='addtask'>Add Task</Link></li>
+          <li className='font-bold'><Link to='addtask'>Add Task</Link></li>
           {users && <>
-            <li><Link to='tasklist'>TaskList</Link></li>
+            <li className='font-bold'><Link to='tasklist'>TaskList</Link></li>
           </>}
-          <li>{users? <button onClick={handleLogOut} >LogOut <span>{users.name}</span></button>:<button><Link to='login'>Login</Link></button>}</li>
+          <li className='font-bold'>{users? <button onClick={handleLogOut} >LogOut <span>{users.name}</span></button>:<button><Link to='login'>Login</Link></button>}</li>
     
     </>
 
 
     return (
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-cyan-500">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52">
+          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[50] p-2 shadow bg-base-100 rounded-box w-52">
             {menuItem}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link to="/" className=" text-xl">Task Management</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
