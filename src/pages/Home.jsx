6c1891d05600283300a933ee701a1d968e35fa5b
JobaerSiddique <jobaersiddique28me@gmail.com>
2024-04-05 00:06:14 +0600

    s

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import groovyWalkAnimation from "../../Animation - 1712253486149.json";
 const Home = () => {
     return (
         <div className='flex flex-col justify-center items-center h-screen p-4 my-10'>
-           <Lottie className='w-[60%]' animationData={groovyWalkAnimation} loop={true} /> 
+           <Lottie className='w-[90%] lg:w-[60%]' animationData={groovyWalkAnimation} loop={true} /> 
            <p className='text-xl lg:text-5xl'>Task Management Web Application</p>
         </div>
     );
